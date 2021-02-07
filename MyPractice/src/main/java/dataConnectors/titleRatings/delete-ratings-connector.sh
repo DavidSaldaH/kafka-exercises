051da3e2b6a32b8681e0503d0a750acdcc19e05a
David <davidsaldarriagaherrera@gmail.com>
2021-02-07 13:53:10 -0500

    Archivos particionados para tener todos los connector y topics generados ademas adicion del connector jcustenborder

--- MyPractice/src/main/java/dataConnectors/titleRatings/delete-ratings-connector.sh
+++ MyPractice/src/main/java/dataConnectors/titleRatings/delete-ratings-connector.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" -X DELETE http://localhost:8083/connectors/tsv_title_basics_spooldir
+curl -H "Content-Type: application/json" -X DELETE http://localhost:8083/connectors/tsv_name_basics_spooldir
